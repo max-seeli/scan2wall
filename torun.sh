5ceff8cd359b64041626a27a15d273cc8da157a0
Andrea Pozzetti <andrea@pozzetti.it>
2025-10-11 15:55:09 +0000

    dh3

--- torun.sh
+++ torun.sh
@@ -23,14 +23,34 @@ source $HOME/miniconda3/bin/activate comfyui
 echo "✅ Miniconda installed and comfyui env active"
 
 #--- Install reqs for ComfyUI and custom nodes ---
-cd ~/ComfyUI
+git clone https://github.com/comfyanonymous/ComfyUI
+
+cd ComfyUI
 pip install -r requirements.txt --upgrade --no-cache-dir
 
 cd custom_nodes
 git clone https://github.com/Comfy-Org/ComfyUI-Manager
 
-cd ComfyUI-Manager
+# --- Install ComfyUI-Manager ---
+git clone https://github.com/Comfy-Org/ComfyUI-Manager
 
+# --- Install custom nodes from git URLs ---
+CUSTOM_NODES=(
+    "https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes"
+    "https://github.com/kijai/ComfyUI-KJNodes"
+    "https://github.com/visualbruno/ComfyUI-Hunyuan3d-2-1/tree/main"
+    "https://github.com/john-mnz/ComfyUI-Inspyrenet-Rembg"
+    # Add more URLs here
+)
+
+echo "📦 Installing custom nodes..."
+for repo in "${CUSTOM_NODES[@]}"; do
+    echo "Cloning $repo"
+    git clone "$repo"
+done
+
+# --- Install dependencies for all custom nodes ---
+python -m custom_nodes/ComfyUI-Manager/cm-cli.py install-deps
 
 echo "🎨 All set! To run ComfyUI:"
 echo "--------------------------------------------"
