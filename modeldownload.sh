7e22a535d97ca5127aabce8b843edf6e75181d1a
Andrea Pozzetti <andrea@pozzetti.it>
2025-10-11 16:36:41 +0000

    model downloading

--- modeldownload.sh
+++ modeldownload.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 set -e  # stop on first error
 
 # Make sure you're in the comfyui environment
@@ -7,6 +6,20 @@ source $HOME/miniconda3/bin/activate comfyui
 # Install comfy-cli if not already installed
 pip install comfy-cli
 
-# Download model (assuming ComfyUI is at ~/scan2wall/ComfyUI)
-cd ~/scan2wall/ComfyUI
-comfy --here model download --url https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors --relative-path models/checkpoints
+# Disable tracking prompt
+export COMFY_CLI_SKIP_PROMPT=1
+
+# Download models (assuming ComfyUI is at ~/scan2wall/ComfyUI)
+cd $HOME/scan2wall/ComfyUI
+
+# Download DIT model
+comfy --here model download \
+  --url "https://huggingface.co/tencent/Hunyuan3D-2.1/resolve/main/hunyuan3d-dit-v2-1/model.fp16.ckpt" \
+  --relative-path models/diffusion_models \
+  --filename hunyuan3d-dit-v2-1-fp16.ckpt
+
+# Download VAE model
+comfy --here model download \
+  --url "https://huggingface.co/tencent/Hunyuan3D-2.1/resolve/main/hunyuan3d-vae-v2-1/model.fp16.ckpt" \
+  --relative-path models/vae \
+  --filename hunyuan3d-vae-v2-1-fp16.ckpt
